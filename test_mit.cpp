a67bf1230dd80d72b292c77ab6e940162efefa22
euyniy <yue_yin@reazon.jp>
2024-06-13 16:40:03 +0900

    Trivial

--- test_mit.cpp
+++ test_mit.cpp
@@ -9,23 +9,32 @@ auto dm = std::make_shared<damiao::Motor>(serial);
 // T_ref = kp*(p_des - theta_m) + kd*(v_des - d_theta_m) + t_ff
 // T_ref = kp*(q - state.q) + kd*(dq - state.dq) + tau
 // iqref = T_ref / KT_OUT
+
+/* When kp=0 and kd≠0, setting v_des will achieve constant speed rotation.
+    There is a steady-state error during constant speed rotation. 
+    Additionally, kd should not be too large as it can cause oscillations.
+    assert kd not too big */
 void velocity_control(int id, float kd, float dq)
 {
-  // When kp=0 and kd≠0, setting v_des will achieve constant speed rotation.
-  // There is a steady-state error during constant speed rotation. 
-  // Additionally, kd should not be too large as it can cause oscillations.
-  // assert kd not too big
+
   dm->control(id, 0, kd, 0, dq, 0);
 }
 
+
+/* When kp=0 and kd=0, a specified torque output can be achieved by setting t_ff. 
+  In this case, the motor will continuously output a constant torque.
+  However, when the motor is idling or under a light load, if t_ff is set too high,
+  the motor will continue to accelerate until it reaches its maximum speed, 
+  and at this point, it still will not achieve the target torque t_ff. */
 void torque_control(int id, float tau)
 {
-  // When kp=0 and kd=0, a specified torque output can be achieved by setting t_ff. 
-  // In this case, the motor will continuously output a constant torque.
-  // However, when the motor is idling or under a light load, if t_ff is set too high,
-  // the motor will continue to accelerate until it reaches its maximum speed, 
-  // and at this point, it still will not achieve the target torque t_ff.
   dm->control(id, 0, 0, 0, 0, tau);
+}
+
+// kd!=0, kp!=0 
+void position_control(int id, float kp, float kd, float q)
+{
+  dm->control(id, kp, kd, q, 0, 0);
 }
 
 void print_motor_state(int id)
@@ -33,12 +42,6 @@ void print_motor_state(int id)
   auto & m = dm->motors[id];
   std::cout << "m" + std::to_string(id) + ": "<< m->state.q << " " << m->state.dq << " " << m->state.tau << std::endl;
   std::cout << "temp(mos): " << m->state.t_mos << " temp(rot)" << m->state.t_rotor << std::endl;
-}
-
-void position_control(int id, float kp, float kd, float q)
-{
-// kd!=0, kp!=0 
-  dm->control(id, kp, kd, q, 0, 0);
 }
 
 int main(int argc , char** argv)
@@ -52,8 +55,9 @@ int main(int argc , char** argv)
   dm->enable(0x02);
   std::cout << "Motor 2 position: " << dm->motors[0x02]->state.q << std::endl;
  
-  //  float kp, float kd, float q, float dq, float tau
-  //        kp,       kd,     pos,      vel,    torque
+  //        id, float kp, float kd, float q, float dq, float tau
+  //            ID,   kp,       kd,     pos,      vel,    torque
+  // dm->control(0x01, 0,        0,       0,        0,         0); 
 
   // velocity (dq) control (kp=0, kd!=0 (not too big))
   velocity_control(0x01, 1.0, 0.5); // kd, dq
